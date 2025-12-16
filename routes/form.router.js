09a8897f11f34b80f592f7a4959f105edc5c011d
Awadhesh Kumar <kumaravi0506@gmail.com>
2025-12-17 01:04:41 +0530

    minor changes in form router

--- routes/form.router.js
+++ routes/form.router.js
@@ -82,7 +82,8 @@ FormRouter.post("/deathForm", async(req, res) => {
         const {name,gender,aadhaar_number,date_of_death,dateOfDeath,place_of_death,
             name_of_mother,name_of_father,mother_aadhaar_number,father_aadhaar_number,
             parent_address_at_death,parent_permanent_address,registration_number,
-            date_of_registration,remarks,date_of_issue,updated_on
+            date_of_registration,remarks,date_of_issue,updated_on,registration_unit_name,
+            registration_unit_code,
         } = req.body;
     
         const existUser = await UserModel.findById(userId);
